f3219abab4f0b42eeb9671d864787ae09638b0dc
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2024-08-16 11:40:45 +0100

    rename summariser to AskRepo, add repo_path and some tests

--- src/ask_repo.py
+++ src/ask_repo.py
@@ -4,17 +4,22 @@ from aider.repo import GitRepo
 from aider.coders import AskCoder
 
 
-class Summariser:
+class AskRepo:
     summary_prompt = "Summarise the purpose and structure of this project. Also list out the most important modules, classes, functions used and their purpose. If any examples, tests and documentation is supplied also summarise that."
 
     def __init__(self, repo_path: str, model_name="ollama/llama3.1:8b") -> None:
         self.io = InputOutput()
         self.repo = GitRepo(io=self.io, fnames=None, git_dname=repo_path)
         self.model = Model(model_name)
+        self.coder = AskCoder(main_model=self.model, io=self.io, repo=self.repo)
 
-    def summary(self) -> str:
-        coder = AskCoder(main_model=self.model, io=self.io, repo=self.repo)
-        summary = coder.run(self.summary_prompt)
+    def get_repo_map(self) -> str:
+        return self.coder.get_repo_map()
+
+    def summarise(self) -> str:
+        summary = self.coder.run(self.summary_prompt)
         return summary
 
-# summary = Summariser("/home/akshaykarle/src/github.com/sahajsoft/pii-detection-and-anonymizer").summary()
+# ask_repo = AskRepo("/home/akshaykarle/src/github.com/sahajsoft/pii-detection-and-anonymizer")
+# print(ask_repo.get_repo_map())
+# print(ask_repo.summarise())
