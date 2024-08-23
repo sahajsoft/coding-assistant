e35af8c00fc396a0fd62e92e3175e181bf46ea2a
mallikarjun-br <93917885+mallikarjun-br@users.noreply.github.com>
2024-08-23 15:09:34 +0100

    refactor weaviate_client

--- libraries/weaviate_client.py
+++ libraries/weaviate_client.py
@@ -3,32 +3,30 @@ import weaviate.classes.config as wc
 import weaviate.classes as wvc
 import sys
 
-def create_client_collection(collection_name: str = "test"):
-    """
 
-    """
-    client = weaviate.connect_to_local()
+def local_client():
+    return weaviate.connect_to_local()
+
+
+def create_or_get_collection(collection_name: str = "test"):
+    client = local_client()
     if not client.collections.exists(name = 'test'):
         client.collections.create(name = 'test')
     collection = client.collections.get('test')
-    return client, collection
+    return collection
 
 def apply_embeddings(embedding_function: callable, embedding_model: str, nodes: list[str]):
     embeddings = []
-
-    prev_message = ""
-
     for i, value in enumerate(nodes):
         res = embedding_function(value, embedding_model)
-        if res is None:
-            continue
-        embedding_i = wvc.data.DataObject(
-            properties={
-                'message': value,
-            },
-            vector=list(res)
-        )
-        embeddings.append(embedding_i)
+        if res is not None:
+            embedding_i = wvc.data.DataObject(
+                properties={
+                    'message': value,
+                },
+                vector=list(res)
+            )
+            embeddings.append(embedding_i)
         # Print the loading percentage
         print(f"\rLoading... {(i+1)*100/len(nodes)}%", end="")
         # Flush the output buffer to ensure the text is printed immediately
