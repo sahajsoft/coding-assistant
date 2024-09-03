60095b43e5314a85c5687652355510f9523dfd1d
Akshay Karle <1443108+akshaykarle@users.noreply.github.com>
2024-09-03 11:30:17 +0100

    add TEST_USE_OPENAI env for running in github actions

--- tests/ask_repo_test.py
+++ tests/ask_repo_test.py
@@ -4,16 +4,25 @@ import os
 from ask_repo import AskRepo
 
 
-def test_invalid_repo_path():
-    with pytest.raises(FileNotFoundError):
-        AskRepo("/non_existent_path")
+class TestAskRepo:
+    @pytest.fixture
+    def model_name(self):
+        if os.getenv("TEST_USE_OPENAI"):
+            return "gpt-3.5-turbo"
+        else:
+            return "ollama/llama3.1:8b"
 
 
-def test_repo_map():
-    ask_repo = AskRepo(os.getcwd())
-    assert "pyproject.toml" in ask_repo.get_repo_map()
+    def test_invalid_repo_path(self):
+        with pytest.raises(FileNotFoundError):
+            AskRepo("/non_existent_path")
 
 
-def test_generates_valid_summary():
-    ask_repo = AskRepo(os.getcwd(), "gpt-3.5-turbo")
-    assert "summary" in ask_repo.summarise()
+    def test_repo_map(self):
+        ask_repo = AskRepo(os.getcwd())
+        assert "pyproject.toml" in ask_repo.get_repo_map()
+
+
+    def test_generates_valid_summary(self, model_name):
+        ask_repo = AskRepo(os.getcwd(), model_name)
+        assert "summary" in ask_repo.summarise()
